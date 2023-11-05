0235159eb3616879f5e3702a95a0dfb8c6d8b099
Derin Atik <104201667+SwachTR@users.noreply.github.com>
2023-11-05 19:42:31 +0300

    Add files via upload

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,62 @@
+import UIKit
+
+// Variables & Constants
+
+// String
+var greeting = "Hello, playground"
+
+var mynumber = 5 + 9
+var mmynuber = 90 * 60
+
+var userName = "James"
+userName.append("Atik")
+userName.lowercased()
+userName.uppercased()
+
+var userSurname = "Atik"
+userName = "Laos"
+
+// integer & doble  & float
+
+// integer
+let userAge = 50
+let myNumber = 4
+userAge / myNumber
+ 
+// doble
+let userAgeD = 50.0
+let myNumberD = 4.0
+userAgeD / myNumberD
+
+// boolean
+var myBoolean = false
+myBoolean = true
+
+var myString : String = "50"
+let anotherNumber : Int = 10
+
+let stringNumber : String = String (20)
+
+// define
+let myVaribale : String
+
+// initialization
+
+myVaribale = "Text"
+myVaribale.uppercased()
+myVaribale.lowercased()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
